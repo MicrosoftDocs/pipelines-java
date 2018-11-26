5e9f51cb1437b4c51bdb95960e36715d9c9f9112
Rishav Sharan <rishav.sharan@microsoft.com>
2018-11-26 20:00:36 +0530

    Adding code coverage for Maven and Gradle projects (#3)
    * Added Code Coverage
    
    * Adding CC to Yaml
    
    * Added Jacoco plugin to build file
    
    * Added Jacoco plugin to build file 2
    
    * Added Jacoco plugin to build file 3
    
    * Added Jacoco plugin to build file 4
    
    * Added Jacoco plugin to build file 5
    
    * Added Jacoco plugin to build file 7
    
    * Added Jacoco based code cov for Maven and Gradle
    
    * Removing the JacocoRpeort bit from yaml

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,11 @@
-import org.junit.*;
+import com.microsoft.demo.Demo;
+import org.junit.Test;
 
 public class MyTest {
     @Test
     public void test_method_1() {
+        Demo d = new Demo();
+        d.DoSomething(true);
     }
 
     @Test
