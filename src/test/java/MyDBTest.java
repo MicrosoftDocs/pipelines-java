fa39b9695ff98578702e02956a0527d194decb0e
Victor Gallet <victor.gallet@protonmail.ch>
2020-03-02 14:36:55 +0100

    Ajout de test avec Catégorie Junit + profile maven

---
+++ src/test/java/MyDBTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import static org.junit.Assert.assertTrue;
+
+@Category(DBTest.class)
+public class MyDBTest {
+
+    @Test
+    public void dbTest() {
+        assertTrue(true);
+    }
+}
--- src/test/java/MyUnitTest.java
+++ src/test/java/MyUnitTest.java
@@ -1,7 +1,10 @@
 import com.microsoft.demo.Demo;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
-public class MyTest {
+@Category(UnitTest.class)
+public class MyUnitTest {
+
     @Test
     public void test_method_1() {
         Demo d = new Demo();
