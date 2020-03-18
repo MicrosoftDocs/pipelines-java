4ebd2520c8e023217a6e7e413fea8c89fd44c691
Vadim Bacalov <bacalov.vadim@gmail.com>
2020-03-18 01:28:35 -0500

    test2

---
+++ src/test/java/MyTest2.java
@@ -0,0 +1,14 @@
+import com.microsoft.demo.Demo;
+import org.junit.Test;
+
+public class MyTest2 {
+    @Test
+    public void test_method_1() {
+        Demo d = new Demo();
+        d.DoSomething(true);
+    }
+
+    @Test
+    public void test_method_2() {
+    }
+}
