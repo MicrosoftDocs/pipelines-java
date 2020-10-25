bf6cf9d01baf628c943c1c4966c550b030052bbf
mtnori <moto44.noriyuki@gmail.com>
2020-10-25 17:35:06 +0900

    fix.

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -2,6 +2,9 @@ package com.microsoft.demo;
 
 public class Demo {
     public void DoSomething(boolean flag){
+        Test t = new Test();
+        System.out.println(t.test(10));
+
         if(flag){
             System.out.println("I am covered");
             return;
---
+++ src/main/java/com/microsoft/demo/Test.java
@@ -0,0 +1,7 @@
+package com.microsoft.demo;
+
+public class Test {
+    int test(int i) {
+        return i + 1;
+    }
+}
