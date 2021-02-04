3bb97a325fb7dd0a7ccdf9e1bf844bcae1c0f369
agrataru <68077772+agrataru@users.noreply.github.com>
2021-02-04 20:09:54 +0530

    adding all the yaml params

--- ymlparser.ps1
+++ ymlparser.ps1
@@ -5,5 +5,16 @@ $content = ''
 foreach ($line in $fileContent) { $content = $content + "`n" + $line }
 $yaml = ConvertFrom-YAML $content
 $accountTypeVal = $yaml.testconfig.accountType
+$customerNameVal = $yaml.testconfig.customerName
+$accountNameVal = $yaml.testconfig.accountName
+$locationVal = $yaml.testconfig.location
+$cloudVal = $yaml.testconfig.cloud
+$emailVal = $yaml.testconfig.email
+$userNameVal = $yaml.testconfig.userName
 Write-Host '##vso[task.setvariable variable=accountType;]'$accountTypeVal
-
+Write-Host '##vso[task.setvariable variable=customerName;]'$customerNameVal
+Write-Host '##vso[task.setvariable variable=accountName;]'$accountNameVal
+Write-Host '##vso[task.setvariable variable=location;]'$locationVal
+Write-Host '##vso[task.setvariable variable=cloud;]'$cloudVal
+Write-Host '##vso[task.setvariable variable=email;]'$emailVal
+Write-Host '##vso[task.setvariable variable=userName;]'$userNameVal
