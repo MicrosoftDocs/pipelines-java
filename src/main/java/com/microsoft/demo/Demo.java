b440ac7b2a277012f9a1b1b5d2fb6d5bd0087ed0
Marius <marius.gheorghies@protonmail.com>
2021-08-31 15:17:49 +0200

    trigger

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -3,10 +3,10 @@ package com.microsoft.demo;
 public class Demo {
     public void DoSomething(boolean flag){
         if(flag){
-            System.out.println("I am covered");
+            System.out.println("I am covered!!");
             return;
         }
 
-        System.out.println("I am not covered");
+        System.out.println("I am not covered!!");
     }
 }
