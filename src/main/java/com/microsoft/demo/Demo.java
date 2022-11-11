2e63f47f246371781a906c74a698d447ef6b853a
Simon Rees <srees@simon-pc.loveadmin.com>
2022-11-11 15:36:51 +0000

    Extended test coverage and applied code style

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -1,12 +1,12 @@
 package com.microsoft.demo;
 
 public class Demo {
-    public void DoSomething(boolean flag){
-        if(flag){
-            System.out.println("I am covered");
-            return;
-        }
-
-        System.out.println("I am not covered");
+  public void DoSomething(boolean flag) {
+    if (flag) {
+      System.out.println("I am covered");
+      return;
     }
+
+    System.out.println("I am not covered");
+  }
 }
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -2,13 +2,15 @@ import com.microsoft.demo.Demo;
 import org.junit.Test;
 
 public class MyTest {
-    @Test
-    public void test_method_1() {
-        Demo d = new Demo();
-        d.DoSomething(true);
-    }
+  @Test
+  public void test_method_1() {
+    Demo d = new Demo();
+    d.DoSomething(true);
+  }
 
-    @Test
-    public void test_method_2() {
-    }
+  @Test
+  public void test_method_2() {
+    Demo d = new Demo();
+    d.DoSomething(false);
+  }
 }
